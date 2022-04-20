a2fe795bb9713d548751461606dcf227aa7d58fa
Declan-Combley <declan-combley@protonmail.com>
2022-04-20 16:11:18 +0800

    Fixed: sepperating arrays and opperators

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use std::env;  
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,51 +8,20 @@ fn main() {
         1 | 2 | 3 => panic!("Expected 3 or more arguments"),
         // all other cases
         _ => {
-            let input: Vec<String> = get_numbers();
-            calculate(input);
-            println!("done...")
+            let mut numbers: Vec<f64> = Vec::new(); 
+            let mut operators: Vec<String> = Vec::new();
+
+            let mut n: usize = 0;
+
+            for arg in env::args() {
+               if n % 2 != 0 { numbers.push(arg.parse().unwrap()) } else { operators.push(arg) }
+               n = n + 1;
+            }
+            operators.remove(0);
+
+            for operator in operators { println!("{:?}", operator) };
+            for number in numbers { println!("{:?}", number) };
+            println!("done...");
         }
     };
-}
-
-fn is_odd(number: usize) -> bool {
-    if number % 2 == 0 { return false; }
-    true
-}
-
-fn get_numbers() -> Vec<String> {
-    let mut input: Vec<String> = Vec::new();
-
-    for argument in env::args() {
-        input.push(argument);
-    }
-    input
-}
-
-fn calculate(input: Vec<String>) {
-    let mut numbers: Vec<f64> = Vec::new();
-    let mut operators: Vec<char> = Vec::new();
-    let mut n = 0;
-
-    for argument in input {
-        if is_odd(n) == true { 
-            let number = argument.parse().unwrap();
-            numbers.push(number);
-        } else {
-            let operator = argument.chars().next().unwrap();
-            operators.push(operator);
-        }
-        n = n + 1;
-    }
-    operators.remove(0);
-    
-    println!("Operators: ");
-    for operator in operators {
-        println!("{:?}", operator);
-    }
-
-    println!("Numbers: ");
-    for number in numbers {
-        println!("{:?}", number);
-    }
 }
