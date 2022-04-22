f07ab6f329240ea6aeecf345dc1d0683ad2d113a
Declan-Combley <declan-combley@protonmail.com>
2022-04-22 19:25:46 +0800

    improved: alot of conventions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;  
 
-fn is_even(x: usize) -> bool {
-    if x % 2 == 0 { return true }
+fn is_even(number: usize) -> bool {
+    if number % 2 == 0 { return true }
     false
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = env::args();
 
     match args.len() {
         // incorrect args given 
@@ -16,18 +16,12 @@ fn main() {
             let mut numbers: Vec<f64> = Vec::new(); 
             let mut operators: Vec<char> = Vec::new();
 
-            let mut n: usize = 0;
-            
-            // pushes all of the operators and numbers into their own vectors
-            // for now this is done based off of the order of input, in that every second 
-            // argument is going to be an operator, but basic algebra would not be particularly co-operative with that idea
-            for arg in env::args() {
-                if is_even(n) == false { numbers.push(arg.parse::<f64>().unwrap()) } 
+            // pushes all of the operators and numbers into their own vectors, based off of the index being odd
+            for (n, arg) in env::args().enumerate() {
+                if !is_even(n) { numbers.push(arg.parse::<f64>().unwrap()) } 
                 else { operators.push(arg.chars().next().unwrap()); }
-                n = n + 1;
             }
-            // for some reason `t` is always the first index
-            operators.remove(0);
+            operators.remove(0); // for some reason `t` is always the first index
 
             order_pairs(numbers, operators);
         }
@@ -41,18 +35,18 @@ fn order_pairs(numbers: Vec<f64>, operators: Vec<char>) {
     
     while n < len - 1 {
         println!("{} {} {}", numbers[n], operators[n], numbers[n + 1]);
-        n = n + 1;
+        n += 1;
     } 
 }
 
 fn calculate_pair(number_1: f64, number_2: f64, operator: char) -> f64 {
     match operator {
-        '+' => return number_1 + number_2,
-        '-' => return number_1 - number_2,
-        'x' => return number_1 * number_2,
-        '/' => return number_1 / number_2,
-        '%' => return number_1 % number_2,
-        '^' => return f64::powf(number_1, number_2),
+        '+' => number_1 + number_2,
+        '-' => number_1 - number_2,
+        'x' => number_1 * number_2,
+        '/' => number_1 / number_2,
+        '%' => number_1 % number_2,
+        '^' => f64::powf(number_1, number_2),
         _ => panic!("Invalid operator"),
     }
 }
