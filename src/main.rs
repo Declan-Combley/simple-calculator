4b4b8912ae336618f668ef593244a3e84bd68292
declan <declan-combley@protonmail.com>
2022-06-03 21:42:41 +0800

    gave up on the order of operations

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,11 @@ fn main() {
     let args = env::args();
 
     match args.len() {
-        // incorrect args given
         1 | 2 | 3 => panic!("Expected 3 or more arguments"),
-        // all other cases
         _ => {
             let mut numbers: Vec<f64> = Vec::new();
             let mut operators: Vec<char> = Vec::new();
 
-            // pushes all of the operators and numbers into their own vectors
             for (n, arg) in env::args().enumerate() {
                 if !is_even(n) {
                     numbers.push(arg.parse::<f64>().unwrap())
@@ -28,27 +25,14 @@ fn main() {
             }
             operators.remove(0); // for some reason `t` is always the first index
 
-            //order_pairs(numbers, operators);
-            //let answer: f64 = calculate(numbers, operators);
-
-            order_pairs(numbers, operators);
-
-            // println!("Answer: {}", answer);
+            let answer: f64 = calculate(numbers, operators);
+            
+            println!("{}", answer);
         }
     };
 }
 
-// TODO: make this function return the values and operators according to the order of operations
-fn order_pairs(_numbers: Vec<f64>, operators: Vec<char>) {
-    for (index, operator) in operators.iter().enumerate() {
-        match operator {
-            'x' | '/' | '%' | '^' => println!("{} needs to move", operators[index]),
-            _ => println!("{}", operators[index]),
-        }
-    }
-}
-
-fn _calculate(mut numbers: Vec<f64>, mut operators: Vec<char>) -> f64 {
+fn calculate(mut numbers: Vec<f64>, mut operators: Vec<char>) -> f64 {
     let len: usize = numbers.len();
     let mut index: usize = 0;
 
